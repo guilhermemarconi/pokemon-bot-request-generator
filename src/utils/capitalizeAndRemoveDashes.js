6d1037e70ca1710a99e67dac008c00700994e883
Guilherme Marconi <guiilhermemarconi@gmail.com>
2023-06-13 23:30:09 -0300

    ✨ get correct species name - resolve #2 (#11)

--- src/utils/capitalizeAndRemoveDashes.js
+++ src/utils/capitalizeAndRemoveDashes.js
@@ -1,4 +1,14 @@
+const edgeCases = {
+  'jangmo-o': 'Jangmo-O',
+  'hakamo-o': 'Hakamo-O',
+  'kommo-o': 'Kommo-O',
+  'mr-mime-galar': 'Mr. Mime Galar',
+  'mr-rime': 'Mr. Rime',
+}
+
 const capitalizeAndRemoveDashes = (str) => {
+  if (edgeCases[str]) return edgeCases[str]
+
   const arr = str.split(/\s|-/);
 
   for (var i = 0; i < arr.length; i++) {
--- src/utils/formatRequestText.js
+++ src/utils/formatRequestText.js
@@ -1,24 +1,16 @@
-/**
- * *trade Appletun @ Shell Bell
- * Ability: Thick Fat
- * Shiny: No 
- * Tera Type: Grass
- * Ball: Friend Ball
- * EVs: 252 HP / 252 SAtk / 6 Spe
- * Modest Nature
- * - Apple Acid
- */
+import getSpeciesRequestName from './getSpeciesRequestName'
 
 const formatRequestText = (data) => {
   const lines = [
-    `${data.botCharacter}trade ${data.species}`,
-    `Ball: ${data.ball}`,
+    `${data.botCharacter}trade ${getSpeciesRequestName(data.species)}`,
   ]
 
+  if (data.gender) lines[0] += ` (${data.gender})`
   if (data.item) lines[0] += ` @ ${data.item}`
   if (data.shiny) lines.push(`Shiny: Yes`)
   if (data.ability) lines.push(`Ability: ${data.ability}`)
   if (data.teraType) lines.push(`Tera Type: ${data.teraType}`)
+  if (data.ball) lines.push(`Ball: ${data.ball}`)
   lines.push(`${data.nature} Nature`)
   if (data.move1) lines.push(`- ${data.move1}`)
   if (data.move2) lines.push(`- ${data.move2}`)
