702128c8c07d4a9ff3af5da1f4b21d17f6e05c6b
Guilherme Marconi <guiilhermemarconi@gmail.com>
2023-06-13 21:04:01 -0300

    :heavy_plus_sign: add @vercel/analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { render } from 'preact'
+import { inject } from '@vercel/analytics'
 import { App } from './app.jsx'
+
 import './index.css'
 
 render(<App />, document.getElementById('app'))
+inject()
